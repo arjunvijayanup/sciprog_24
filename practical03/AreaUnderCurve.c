97b432d97e05057649bf03301328a483c4c8c5ff
Arjun Vijay Anup <arjun.vijayanup@gmail.com>
2024-10-07 20:19:16 +0100

    Adding practical03 codes

---
+++ practical03/AreaUnderCurve.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+	// Initialize variables
+	float a,b,fx,sum,n,h,x;
+	int i;
+
+	// Setting the integration limits and no. of divisions
+	a=0;
+	b=M_PI/3;
+	n=12;
+	h=(b-a)/n;
+
+	// Sum of first part tan(0) and tan (pi/3)
+	sum=tan(a)+tan(b);
+
+	// Trapezoidal Rule: For-loop for calculating the sum of all other tan values
+	for(i=1;i<n;i++){
+
+		x=a+(i*h);
+		sum+=2*tan(x);
+
+		}
+
+	// Final calculation of area
+	fx=(h/2)*sum;
+
+	//Print the final values
+	printf("Area under the curve for tan(x) from 0 to PI/3: %f\n",fx);
+	printf("Value of log(2): %f\n",log(2));
+
+	return 0;
+
+}
+
+
+
+
---
+++ practical03/README.md
@@ -0,0 +1,9 @@
+This is the folder which contains files for Practical 3
+
+## Compile
+
+* gcc AreaUnderCurve.c -o AreaUnderCurve -lm
+
+## Execute
+
+* ./AreaUnderCurve.c
