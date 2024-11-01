6a1d4825bb857b1cbd631199175f0ed2067e7f4f
Arjun Vijay Anup <arjun.vijayanup@gmail.com>
2024-11-01 16:49:50 +0000

    Adding practical06 codes

---
+++ practical06/README.md
@@ -0,0 +1,36 @@
+# Practical 6
+
+This folder contains the practical 06 codes
+
+## Compile
+
+```bash
+gcc -o matmul matmultiply.c
+```
+### In Makefile folder (using makefile method)
+
+```bash
+gcc main.c mmultiply.c -o matmulmf
+```
+- OR
+
+```bash
+make
+```
+* Make will create the executable mmc
+
+## Execute
+
+```bash
+./matmul
+```
+### In Makefile folder (using makefile method)
+
+```bash
+./matmulmf
+```
+- OR
+
+```bash
+./mmc
+```
---
+++ practical06/makefile/main.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+//Function initialisation
+void matmultiply(int n, int p, int q, double A[n][p], double B[p][q], double C[n][q]);
+
+int main(){
+
+    int n=5, p=3, q=4;
+    double A[n][p], B[p][q], C[n][q];
+
+    //Intialising A
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p;j++){
+            A[i][j]=i+j;
+        }
+    }
+
+    //Intialising B
+    for(int i=0;i<p;i++){
+        for(int j=0;j<q;j++){
+            B[i][j]=i-j;
+        }
+    }
+
+    //Intialising C
+    for(int i=0;i<n;i++){
+        for(int j=0;j<q;j++){
+            C[i][j]=0.0;
+        }
+    }
+
+    //Matrix Multiplication
+    matmultiply(n, p, q, A, B, C);
+
+/*  for(int i=0;i<n;i++){
+        for(int j=0;j<q;j++){
+            for(int k=0;k<=p-1;k++){
+                C[i][j]+=A[i][k]*B[k][j];
+                }
+
+        }
+    }
+*/
+    //Printing out the matrix A
+    printf("Matrix A:\n\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p;j++){
+            printf("%3.0lf\t",A[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    //Printing out the matrix B
+    printf("Matrix B:\n\n");
+    for(int i=0;i<p;i++){
+        for(int j=0;j<q;j++){
+            printf("%3.0lf\t",B[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    //Printing out the matrix C
+    printf("Resulting Matrix C after multiplication:\n\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<q;j++){
+            printf("%3.0lf\t",C[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+
+    return 0;
+
+
+}
